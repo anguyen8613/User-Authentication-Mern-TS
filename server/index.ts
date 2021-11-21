6adb1bcd3ee52d94a652a5513aa0b96133168435
unknown <anguyen@softechiq.com>
2021-11-21 12:06:54 -0800

    change cors origin

--- server/index.ts
+++ server/index.ts
@@ -18,10 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:3000',
-      'https://user-authentication-mern-ts.vercel.app',
-    ],
+    origin: '*',
     credentials: true,
   })
 );
